d9365192cb7240fa0053090e96a9f4988d18ab14
kmunene <kmunene529@gmail.com>
2024-06-09 03:36:04 +0300

    listening to buruklyn boys while coding this

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    pascals triangle
+    finding the solution had me pulling my hair out
+"""
+
+
+def pascal_triangle(n):
+    """ The function returns pascals triangle columns """
+
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for row_num in range(n):
+        row = [1]
+
+        if row_num > 0:
+            for i in range(1, row_num):
+                numb = triangle[row_num - 1][i - 1] + triangle[row_num - 1][i]
+                row.append(row_numb)
+            row.append(1)
+
+        triangle.append(row)
+
+    return triangle
